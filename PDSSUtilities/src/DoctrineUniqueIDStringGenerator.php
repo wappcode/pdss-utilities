ee1e3057726784b82b981b207b15203381924534
Jesus Abarca <jesus.abarca.g@gmail.com>
2025-03-14 00:18:00 -0600

    Actualización DoctrineUniqueIdStringGenerator

--- PDSSUtilities/src/DoctrineUniqueIDStringGenerator.php
+++ PDSSUtilities/src/DoctrineUniqueIDStringGenerator.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Id\AbstractIdGenerator;
 
 class DoctrineUniqueIDStringGenerator extends AbstractIdGenerator
 {
-    public function generateId(EntityManagerInterface $em, object|null $entity): mixed
+    public function generate(EntityManagerInterface $em,  $entity): mixed
     {
         $randomStr = $this->randomString(3);
         $prefix =  rand() . "__";
