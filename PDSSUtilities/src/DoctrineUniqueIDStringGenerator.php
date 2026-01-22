ced33bd47783eb6cdee51cd5620c88011d94e79f
JJesus Abarca González <j.abarca@qualem.com>
2026-01-22 16:30:33 -0600

    Se cambio la forma en que se genera un id de string

--- PDSSUtilities/src/DoctrineUniqueIDStringGenerator.php
+++ PDSSUtilities/src/DoctrineUniqueIDStringGenerator.php
@@ -9,11 +9,15 @@ class DoctrineUniqueIDStringGenerator extends AbstractIdGenerator
 {
     public function generateId(EntityManagerInterface $em, object|null $entity): mixed
     {
-        $randomStr = $this->randomString(3);
-        $prefix =  rand() . "__";
-        return $randomStr . md5(uniqid($prefix, true));
+        return $this->uuidv4();
     }
 
+    /**
+     *
+     * @param integer $howmany
+     * @return void
+     * @deprecated  version 5.0.0 Use uuidv4() instead.
+     */
     protected function randomString($howmany = 6)
     {
         $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
@@ -24,5 +28,12 @@ class DoctrineUniqueIDStringGenerator extends AbstractIdGenerator
             $str .= $letters[--$index];
         }
         return $str;
+    }
+    protected  function uuidv4(): string
+    {
+        $data = random_bytes(16);
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40); // set version to 0100
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
